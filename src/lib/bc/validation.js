838fff4f167de4299db79f554131280b5d31e101
iwbinb <34509975+iwbinb@users.noreply.github.com>
2018-10-24 22:32:57 +0800

    Update validation.js

--- src/lib/bc/validation.js
+++ src/lib/bc/validation.js
@@ -72,12 +72,12 @@ function isValidBlock(newBlock, type = 0) {
     //return false
   }
   let btcHeight = newBlock.getBlockchainHeaders().getBtcList()[0].getHeight();
-  if (btcHeight > 553300) {
+  if (btcHeight > 550000) {
     logger.warn('rejecting shitty btc block with height=' + btcHeight);
     return false;
   }
   let ethHeight = newBlock.getBlockchainHeaders().getEthList()[0].getHeight();
-  if (ethHeight > 6789430) {
+  if (ethHeight > 6610000) {
     logger.warn('rejecting shitty eth block with height=' + ethHeight);
     return false;
   }
