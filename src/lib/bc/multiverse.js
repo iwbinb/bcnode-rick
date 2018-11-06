41a2d40bfc16722fe12ce83803514af762361c08
iwbinb <34509975+iwbinb@users.noreply.github.com>
2018-11-06 08:46:34 +0800

    Update multiverse.js

--- src/lib/bc/multiverse.js
+++ src/lib/bc/multiverse.js
@@ -574,12 +574,12 @@ class Multiverse {
       const receivedBlocks = flatten(Object.values(block.getBlockchainHeaders().toObject()));
 
       let btcHeight = block.getBlockchainHeaders().getBtcList()[0].getHeight()
-      if (btcHeight > 550000) {
+      if (btcHeight > 552000) {
         this._logger.warn('rejecting shitty btc block with height=' + btcHeight)
         return false
       }
       let ethHeight = block.getBlockchainHeaders().getEthList()[0].getHeight()
-      if (ethHeight > 6680000) {
+      if (ethHeight > 6690000) {
         this._logger.warn('rejecting shitty eth block with height=' + ethHeight)
         return false
       }
